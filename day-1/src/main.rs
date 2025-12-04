6336d51b7da7786dcdf9a5ba730a5625db6c0cdd
Marsango <gmarsango@gmail.com>
2025-12-04 19:20:24 -0300

    day 1 puzzle 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -4,25 +4,53 @@ struct Dial {
     starting_point: i32,
     zero_count: i32
 }
+// Start pointing to 50, zero_count = 0
+// L350
+// 350 > 99
+// self.zero_count += 3
+// self.starting_point = 0
 
+// ways to point to 0;
+// first way: instruction + self.starting_point > 99;
+// second way: - instruction == self.starting_point;
+// third way: -instruction > self.starting_point;
 impl Dial {
     fn rotate(&mut self, mut instruction: i32) {
         if instruction.abs() > 99 {
+            self.zero_count += instruction.abs() / 100;
             instruction = instruction % 100;
         }
 
+        if instruction == 0 {
+            return;
+        }
+
+        if self.starting_point == -instruction {
+            self.zero_count += 1;
+        } else if self.starting_point + instruction < 0 && self.starting_point != 0{
+            self.zero_count += 1;
+        } else if self.starting_point + instruction > 99 {
+            self.zero_count += 1;
+        }
+
         self.starting_point += instruction;
+
         if self.starting_point < 0 {
             self.starting_point += 100;
         } else if self.starting_point > 99 {
             self.starting_point -= 100;
         }
-        println!("Current value: {} | {} | {}", self.starting_point, instruction, self.zero_count);
-        if self.starting_point == 0 {
-            self.zero_count += 1;
-        }
+        println!("{} | {}", self.starting_point, self.zero_count);
     }
 }
+
+// Im counting too much points to zero
+// Im counting 1 per time that instruction is bigger than 100.
+// Example: 300 will add 3 to the counter;
+
+// Im counting 1 per time that starting_point became less than zero
+
+// Im counting 1 per time that starting_point became greater than 99
 
 fn parse_instruction(instruction: &str) -> i32 {
     let direction = instruction.chars().nth(0).expect("Error while reading the string");
